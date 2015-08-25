2319efb0f10c73b1ea40db1993b5e2d0925dbee2
p-gnativ <p.gnativ@angistudio.com>
2015-08-25 15:14:25 +0300

    fixed menu

--- wp-content/themes/soa/functions.php
+++ wp-content/themes/soa/functions.php
@@ -82,7 +82,7 @@ function as_logo_customize_register($wp_customize){
 add_action('customize_register', 'as_logo_customize_register', 11);
 
 function as_lang_menu_item($items, $args){
-	if(class_exists( 'MslsOutput' ) && 'main' == $args->theme_location){
+	if(class_exists('MslsOutput')){ // && 'main' == $args->theme_location
 		$obj = new MslsOutput;
 		$arr = $obj->get(1);
 		if(!empty($arr)){
@@ -94,100 +94,99 @@ function as_lang_menu_item($items, $args){
 add_filter('wp_nav_menu_items', 'as_lang_menu_item', 10, 2);
 
 function cpcm_replace_taxonomy_by_posts1($sorted_menu_items, $args){
-	$result = array();
-	$inc = 0;
-	$menu_item_parent_map = array();
-	foreach((array) $sorted_menu_items as $key => $menu_item){
-		//var_dump($menu_item);
-		$menu_item->menu_order = $menu_item->menu_order + $inc;
-		if($menu_item->type == 'taxonomy'){
-			//
-			$query_arr = array();
-			$query_arr['tax_query'] = array(
-				'relation' => 'AND',
-				array(
-					'taxonomy'=>$menu_item->object,
-					'field'=>'id',
-					'terms'=>$menu_item->object_id
-				)
-			);
+	if($args->theme_location == 'main'){
+		return $sorted_menu_items;
+	}else{
+		$result = array();
+		$inc = 0;
+		$menu_item_parent_map = array();
+		foreach((array) $sorted_menu_items as $key => $menu_item){
+			$menu_item->menu_order = $menu_item->menu_order + $inc;
+			if($menu_item->type == 'taxonomy'){
+				//
+				$query_arr = array();
+				$query_arr['tax_query'] = array(
+					'relation' => 'AND',
+					array(
+						'taxonomy'=>$menu_item->object,
+						'field'=>'id',
+						'terms'=>$menu_item->object_id
+					)
+				);
+
+				$tag = get_taxonomy($menu_item->object);
+
+				$query_arr['post_type'] = $tag->object_type;
 
 
-			$tag = get_taxonomy($menu_item->object);
+				$posts = get_posts($query_arr);
+				//var_dump($posts);
+				$menu_item_parent = $menu_item->menu_item_parent;
+				// empty not show
+				if(empty($posts)){
+					$inc -= 1;
+					$menu_item_parent_map[$menu_item->db_id] = $menu_item->menu_item_parent;
+				}else{
+					array_push($result, $menu_item);
+				}
 
-			$query_arr['post_type'] = $tag->object_type;
+				$current_parent_id = $menu_item->menu_item_parent;
 
-
-			$posts = get_posts($query_arr);
-			//var_dump($posts);
-			$menu_item_parent = $menu_item->menu_item_parent;
-			// empty not show
-			if(empty($posts)){
-				$inc -= 1;
-				$menu_item_parent_map[$menu_item->db_id] = $menu_item->menu_item_parent;
-			}else{
-				array_push($result, $menu_item);
-			}
-
-			$current_parent_id = $menu_item->menu_item_parent;
-
-			while(array_key_exists(strval($current_parent_id), $menu_item_parent_map) == 1){
-				$current_parent_id = $menu_item_parent_map[$current_parent_id];
-			}
-			$menu_item->menu_item_parent = $current_parent_id;
-			//var_dump($menu_item->menu_item_parent);
-			foreach((array) $posts as $pkey => $post){
-				$post = wp_setup_nav_menu_item( $post );
-				$current_parent_id = $menu_item->db_id;
 				while(array_key_exists(strval($current_parent_id), $menu_item_parent_map) == 1){
 					$current_parent_id = $menu_item_parent_map[$current_parent_id];
 				}
-				$post->menu_item_parent = $current_parent_id;
-				// properties from the old to the new one
-				$post->target = $menu_item->target;
-				$post->classes = array_merge( $post->classes, (array) get_post_meta($menu_item->db_id, "_menu_item_classes", true) ); // copy custom css classes that the user specified under "CSS Classes (optional)"
-				$post->xfn = $menu_item->xfn;
-				$post->description = $menu_item->description;
-				// title of the new menu item
-				//$post->title = get_post_meta($menu_item->db_id, "_cpcm-item-titles", true);
-				// Replace the placeholders in the title by the properties of the post
-				//$post->title = $this->replace_placeholders($post, $post->title);
-				$inc += 1;
-				$post->menu_order = $menu_item->menu_order + $inc;
-				// Extend the items with classes.
+				$menu_item->menu_item_parent = $current_parent_id;
+				//var_dump($menu_item->menu_item_parent);
+				foreach((array) $posts as $pkey => $post){
+					$post = wp_setup_nav_menu_item( $post );
+					$current_parent_id = $menu_item->db_id;
+					while(array_key_exists(strval($current_parent_id), $menu_item_parent_map) == 1){
+						$current_parent_id = $menu_item_parent_map[$current_parent_id];
+					}
+					$post->menu_item_parent = $current_parent_id;
+					// properties from the old to the new one
+					$post->target = $menu_item->target;
+					$post->classes = array_merge( $post->classes, (array) get_post_meta($menu_item->db_id, "_menu_item_classes", true) ); // copy custom css classes that the user specified under "CSS Classes (optional)"
+					$post->xfn = $menu_item->xfn;
+					$post->description = $menu_item->description;
+					// title of the new menu item
+					//$post->title = get_post_meta($menu_item->db_id, "_cpcm-item-titles", true);
+					// Replace the placeholders in the title by the properties of the post
+					//$post->title = $this->replace_placeholders($post, $post->title);
+					$inc += 1;
+					$post->menu_order = $menu_item->menu_order + $inc;
+					// Extend the items with classes.
 
 
 
+				}
+				_wp_menu_item_classes_by_context( $posts );
+				$result = array_merge( $result, $posts );
 			}
-			_wp_menu_item_classes_by_context( $posts );
-			$result = array_merge( $result, $posts );
-		}
-		else
-		{
-			// Other objects may have a parent that has been removed by cpcm. Fix that here.
-			// Set the menu_item_parent for the menu_item: If the parent item was removed, go up a level
-			$current_parent_id = $menu_item->menu_item_parent;
-			//var_dump($menu_item_parent_map);
-			while (array_key_exists(strval($current_parent_id), $menu_item_parent_map) == 1)
+			else
 			{
-				$current_parent_id = $menu_item_parent_map[$current_parent_id];
+				// Other objects may have a parent that has been removed by cpcm. Fix that here.
+				// Set the menu_item_parent for the menu_item: If the parent item was removed, go up a level
+				$current_parent_id = $menu_item->menu_item_parent;
+				//var_dump($menu_item_parent_map);
+				while (array_key_exists(strval($current_parent_id), $menu_item_parent_map) == 1)
+				{
+					$current_parent_id = $menu_item_parent_map[$current_parent_id];
+				}
+				$menu_item->menu_item_parent = $current_parent_id;
+
+				// Treat other objects as usual, but note that the position
+				// of elements in the array changes.
+				array_push($result,$menu_item);
 			}
-			$menu_item->menu_item_parent = $current_parent_id;
 
-			// Treat other objects as usual, but note that the position
-			// of elements in the array changes.
-			array_push($result,$menu_item);
+
 		}
-
-
+		unset($sorted_menu_items);
+		unset($menu_item_parent_map);
+		_wp_menu_item_classes_by_context($result);
+		return $result;
 	}
-	unset( $sorted_menu_items );
-    unset( $menu_item_parent_map );
-    _wp_menu_item_classes_by_context( $result );
-
-    		return $result;
-	//var_dump($sorted_menu_items);
-
-	} // function
+}
 
 add_filter( 'wp_nav_menu_objects', 'cpcm_replace_taxonomy_by_posts1', 1, 2 );
